94f9fc005c4e49c5ea3a8962e9ca203a4f459c5e
raytlty <alexmao930918@gmail.com>
2021-07-30 08:21:00 +0000

    upgrade RewardPool

--- scripts/rinkeby/upgrade-rewardpool.ts
+++ scripts/rinkeby/upgrade-rewardpool.ts
@@ -1,11 +1,11 @@
 const { ethers, upgrades } = require('hardhat');
 
-const RewardPoolAddr = '0x9AF53485b8BC4Ff807CF1F5564c55fE12F019c96'
+const RewardPoolAddr = '0xF5e3e76Da5423BafD8764FB5aF0AaC7398a8574C'
 
 async function main() {
     const factory = await ethers.getContractFactory('RewardPool');
     const rewardPool = await upgrades.upgradeProxy(RewardPoolAddr, factory);
-    console.log('Upgrade RewardPool') // 0x9AF53485b8BC4Ff807CF1F5564c55fE12F019c96
+    console.log('Upgrade RewardPool')
     console.log(rewardPool.address)
 }
 
